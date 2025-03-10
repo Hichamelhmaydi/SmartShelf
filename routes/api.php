ecae4f6acf509fbe638aa99e400ce233429c0f70
Hichamelhmaydi <ct1594065@gmail.com>
2025-03-10 23:14:15 +0000

    ajouter la fonction de authentification avec sanctume

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserAuthController;
 
-Route::get('/user', function (Request $request) {
+
+Route::post('register', [UserAuthController::class, 'register']);
+Route::post('login', [UserAuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('logout', [UserAuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
